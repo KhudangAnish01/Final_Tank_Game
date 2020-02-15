580b5e577644fbcf5430d2995788e7c56a3322d7
AnishKhudang001 <aniskhudlim@gmail.com>
2020-02-15 15:23:58 +0545

    SideWayForce-Friction-C++

--- BattleTank_Final/Source/BattleTank_Final/Private/TankTrack.cpp
+++ BattleTank_Final/Source/BattleTank_Final/Private/TankTrack.cpp
@@ -1,7 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TankTrack.h"
+
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{//each tick it place opposite force to stop slippage if angle between GetRightVector and GetComponentVelocity is 0 and 180 i.e(cosine law)
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	//required acceleration
+	auto CorrectioAcceleration = -SlippageSpeed / DeltaTime* GetRightVector();//here a=(change of velocity/change of time).We multiply it with getrightvector cause tank need to move toward right
+	
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	//applied sideway force
+	auto SideWayForce = TankRoot->GetMass() * CorrectioAcceleration/2;
+	TankRoot->AddForce(SideWayForce);
+}
 
 void  UTankTrack::SetThrottle(float Throttle)
 {
